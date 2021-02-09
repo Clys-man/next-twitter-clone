a2115a33333ce749f33f1971bf53fcd9c25f28f0
clys-man <clysmanalves@gmail.com>
2021-02-09 11:52:18 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Head from 'next/head'
+
+
+const Home: React.FC = () => {
+  return (
+    <Head>
+      <title>Template NextJs</title>
+    </Head>
+  )
+}
+
+export default Home
