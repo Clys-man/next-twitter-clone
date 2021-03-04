bcc452e78bafa9bee923ed04f3a0c7e1780fd155
Clys-man <clysmanalves@gmail.com>
2021-03-04 02:00:48 -0300

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,10 +5,6 @@ const Home: React.FC = () => {
   return (
     <div id="app">
       <SEO title="teste" image="boost.png" shouldExcludeTitleSuffix />
-      <main>
-        <h1>NextJs Template</h1>
-        <h3>By: @clys_man</h3>
-      </main>
     </div>
   )
 }
